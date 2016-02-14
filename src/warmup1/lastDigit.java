c6cd2e6cc6530adee16860c1bb552a6334a35616
Raymart <raymart54@gmail.com>
2016-02-14 12:12:24 -0500

    I guess the other approach is simple and easy to understand for others since it checks the param if both are the same if not then add directly and return.  Learn to use a variable to manipulate. Less use of condition e.g. <,>,>=, etc.

---
+++ src/warmup1/lastDigit.java
@@ -0,0 +1,44 @@
+package warmup1;
+
+/**
+ * Created by RAYMART on 2016-02-14.
+ */
+public class LastDigit {
+    public static void main(String[] args) {
+        System.out.println(lastDigit(10, 0));
+    }
+
+    public static boolean lastDigit(int a, int b) {
+        int c;
+        int d;
+        if (a >= 10) {
+            c = a % 10;
+            if (b < 10) {
+                if (c == b) {
+                    return true;
+                }
+            }
+        }
+        if (b >= 10) {
+            c = b % 10;
+            if (a < 10) {
+                if (c == a) {
+                    return true;
+                }
+            }
+        }
+        if ((a >= 10 && b >= 10) || (a == b)) {
+            c = a % 10;
+            d = b % 10;
+            if (c == d) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // There is also the very short version of this
+    // return (a % 10 == b % 10)
+    // that's it
+
+}
