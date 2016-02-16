c788f108024e9e5aee91f5c29be6dcb849a89e2f
Raymart <raymart54@gmail.com>
2016-02-16 18:15:46 -0500

    StringTimes done!

---
+++ src/warmup2/StringTimes.java
@@ -0,0 +1,24 @@
+package warmup2;
+
+/**
+ * Created by RAYMART on 2016-02-16.
+ */
+public class StringTimes {
+    public static void main(String[] args) {
+        System.out.println(stringTimes("hi", 3));
+    }
+
+    public static String stringTimes(String str, int n) {
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            //res = res + str;
+            // this is add and assignment operator.
+            // +=
+            res += str;
+        }
+
+
+        //System.out.println(str);
+        return res;
+    }
+}
