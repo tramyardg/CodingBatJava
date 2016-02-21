8032a9bb166339904185bb32d37aeff0f842bab3
Raymart <raymart54@gmail.com>
2016-02-21 15:15:55 -0500

    New package array1!

---
+++ src/array1/MiddleWay.java
@@ -0,0 +1,7 @@
+package array1;
+
+/**
+ * Created by RAYMART on 2016-02-21.
+ */
+public class MiddleWay {
+}
---
+++ src/fizzbuzz/FizzString2.java
@@ -0,0 +1,24 @@
+package fizzbuzz;
+
+/**
+ * Created by RAYMART on 2016-02-21.
+ */
+public class FizzString2 {
+    public static void main(String[] args) {
+        System.out.println(fizzString2(23));
+        System.out.println((15 % 3) == 0);
+    }
+
+    public static String fizzString2(int n) {
+        if ((n % 3) == 0 && (n % 5) == 0) {
+            return "FizzBuzz";
+        }
+        if ((n % 3) == 0) {
+            return "Fizz";
+        }
+        if ((n % 5) == 0) {
+            return "Buzz";
+        }
+        return n + "!";
+    }
+}
