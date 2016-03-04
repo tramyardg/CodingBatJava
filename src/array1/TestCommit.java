3c092d64ba88ed9b2063df8f56b040adee891f72
Raymart De Guzman <tramyardg@gmail.com>
2016-03-04 00:07:45 -0500

    Testing commit~

---
+++ src/array1/TestCommit.java
@@ -0,0 +1,10 @@
+package array1;
+
+/**
+ * Created by RAYMARTHINKPAD on 2016-03-04.
+ */
+public class TestCommit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
