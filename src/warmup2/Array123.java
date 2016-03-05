1b2c9b44d4fe17a2aa91eff32d579b3155c717c5
Raymart De Guzman <raymart54@gmail.com>
2016-03-05 15:15:01 -0500

    Update Array123.java

--- src/warmup2/Array123.java
+++ src/warmup2/Array123.java
@@ -6,6 +6,8 @@ package warmup2;
 public class Array123 {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 3, 1};
+        int[] data =  {5, 2,1,2,3,5,8,10,6,7};
+    int[] data2 = {2,2,4,5,1,2,3};
         System.out.println(array123(arr));
     }
 
@@ -28,4 +30,40 @@ public class Array123 {
         }
         return false;
     }
+    /*
+    public static boolean array123 (int[] a) {
+ // Note: iterate < length-2, so can use i+1 and i+2 in the loop
+//  for (int i=0; i < (nums.length-2); i++) {
+//    if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3) return true;
+//  }
+//  return false;    
+    
+   boolean isTrue = false;
+   int maxIndex =  a.length;
+   int correctValue = 1;
+   int biggestNumber = 4;
+   
+   for (int i=0; i < a.length; i ++) {
+       if (a.length < biggestNumber) break;  // check array length size
+       
+       if ((i == a.length-1) && a[i] < biggestNumber) {  // check the last array content
+           isTrue = false;
+           break;
+       
+       }
+       if (a[i] == correctValue) {
+            if (biggestNumber == a[i]) {
+               break;
+            }
+            isTrue = true;
+            correctValue++;
+            
+       } else {
+            isTrue = false;
+            correctValue = a[i] +1;
+       }
+   }
+   return isTrue;
+}
+    */
 }
