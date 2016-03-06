3387afdaa5e5c02c09a5e59c10c4671eeefe6046
Raymart De Guzman <tramyardg@gmail.com>
2016-03-05 21:35:58 -0500

    Array123 is now dynamic.

--- src/warmup2/Array123.java
+++ src/warmup2/Array123.java
@@ -6,64 +6,55 @@ package warmup2;
 public class Array123 {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 3, 1};
-        int[] data =  {5, 2,1,2,3,5,8,10,6,7};
-    int[] data2 = {2,2,4,5,1,2,3};
-        System.out.println(array123(arr));
+        int[] data = {5, 2, 1, 2, 3, 5, 8, 10, 6, 7};
+        int[] data2 = {5, 1, 2, 3};
+        System.out.println(array123(data));
     }
 
+    /**
+     * public static boolean array123(int[] a) {
+     * <p>
+     * <p>
+     * if (a.length >= 3) {
+     * // -2 prevents index out of bounds
+     * for (int i = 0; i <= a.length - 2; i++) {
+     * if (a[i] == 1) {
+     * if (a[i + 1] == 2) {
+     * if (a[i + 2] == 3) {
+     * return true;
+     * }
+     * }
+     * }
+     * }
+     * }
+     * return false;
+     * }
+     */
     public static boolean array123(int[] a) {
-        /**
-         *  Very important:
-         *       Note: iterate < length-2, so can use i+1 and i+2 in the loop
-         */
-        if (a.length >= 3) {
-            // -2 prevents index out of bounds
-            for (int i = 0; i <= a.length - 2; i++) {
-                if (a[i] == 1) {
-                    if (a[i + 1] == 2) {
-                        if (a[i + 2] == 3) {
-                            return true;
-                        }
-                    }
+        boolean cond = false;
+        int initialVal = 1;
+        int endingVal = 3;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a.length < endingVal) break;  // check array length size
+
+            if ((i == a.length - 1) && a[i] < endingVal) {  // check the last array content
+                cond = false;
+                break;
+            }
+            if (a[i] == initialVal) {
+                if (endingVal == a[i]) {
+                    break;
                 }
+                cond = true;
+                initialVal++;
+
+            } else {
+                cond = false;
+                initialVal = a[i] + 1;
             }
         }
-        return false;
+        return cond;
     }
-    /*
-    public static boolean array123 (int[] a) {
- // Note: iterate < length-2, so can use i+1 and i+2 in the loop
-//  for (int i=0; i < (nums.length-2); i++) {
-//    if (nums[i]==1 && nums[i+1]==2 && nums[i+2]==3) return true;
-//  }
-//  return false;    
-    
-   boolean isTrue = false;
-   int maxIndex =  a.length;
-   int correctValue = 1;
-   int biggestNumber = 4;
-   
-   for (int i=0; i < a.length; i ++) {
-       if (a.length < biggestNumber) break;  // check array length size
-       
-       if ((i == a.length-1) && a[i] < biggestNumber) {  // check the last array content
-           isTrue = false;
-           break;
-       
-       }
-       if (a[i] == correctValue) {
-            if (biggestNumber == a[i]) {
-               break;
-            }
-            isTrue = true;
-            correctValue++;
-            
-       } else {
-            isTrue = false;
-            correctValue = a[i] +1;
-       }
-   }
-   return isTrue;
-}
-    */
+
 }
