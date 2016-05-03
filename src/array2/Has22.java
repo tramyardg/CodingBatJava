fc4b58259a75b134c09cc6133d56d91ff1184181
Raymart De Guzman <tramyardg@gmail.com>
2016-05-03 14:15:26 -0400

    CodingBat update!

---
+++ src/array2/Has22.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array2;
+
+/**
+ *
+ * @author RAYMARTHINKPAD
+ */
+public class Has22 {
+
+    public boolean has22(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
