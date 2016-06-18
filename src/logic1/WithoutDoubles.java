d747e70eb3cf524f17e9d0e5d51d6e9ca8e347c8
Raymart De Guzman <tramyardg@gmail.com>
2016-06-17 20:30:54 -0400

    WithoutDoubles logic 1 completed.

--- src/logic1/WithoutDoubles.java
+++ src/logic1/WithoutDoubles.java
@@ -5,5 +5,27 @@ package logic1;
  * @author RAYMARTHINKPAD
  */
 public class WithoutDoubles {
-    
+
+    public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        int ret = 0;
+        int sumDie1n2 = die1 + die2;
+        if (sumDie1n2 < 6 && noDoubles == true || noDoubles == false) {
+            ret = sumDie1n2;
+        } else {
+            ret = sumDie1n2;
+        }
+        if (die1 == die2 && noDoubles == false) {
+            ret = sumDie1n2;
+        }
+        if (die1 == die2 && noDoubles == true) {
+            if (die1 == 6 && die2 == 6) {
+                ret = die1 + 1;
+            } else {
+                ret = sumDie1n2 + 1;
+            }
+        }
+        return ret;
+
+    }
+
 }
