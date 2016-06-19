6221cdcc015bcfddcd2934563ce198eeb3630bb0
Raymart De Guzman <tramyardg@gmail.com>
2016-06-18 20:37:11 -0400

    MakeAbba completed string1.

---
+++ src/string1/MakeAbba.java
@@ -0,0 +1,14 @@
+package string1;
+
+/**
+ *
+ * @author RAYMARTHINKPAD
+ */
+public class MakeAbba {
+
+    public String makeAbba(String a, String b) {
+        String newStr = a+b+b+a;
+        return newStr;
+    }
+
+}
