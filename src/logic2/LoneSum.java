418fd78b1bd200b6007819d21e449d1657640e66
Raymart De Guzman <raymart54@gmail.com>
2016-06-22 22:01:31 -0400

    Formatted LoneSum code.

--- src/logic2/LoneSum.java
+++ src/logic2/LoneSum.java
@@ -8,17 +8,17 @@ public class LoneSum {
     public static void main(String[] args) {
         System.out.println(loneSum(3,3,1));
     }
-    public static  int loneSum(int a, int b, int c) {
-        if(a!=b&&b!=c&&a!=c) {
-            return a+b+c;
+    public static int loneSum(int a, int b, int c) {
+        if (a != b && b != c && a != c) {
+            return a + b + c;
         }
-        if(a==b&&c!=a&&c!=b) {
+        if (a == b && c != a && c != b) {
             return c;
         }
-        if(b==c&&a!=b&&a!=c) {
+        if (b == c && a != b && a != c) {
             return a;
         }
-        if(a==c&&b!=a&&b!=c) {
+        if (a == c && b != a && b != c) {
             return b;
         }
         return 0;
