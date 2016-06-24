7dc4d80c557d4cfdb610f5bd2997f731d896376b
Raymart De Guzman <raymart54@gmail.com>
2016-06-24 09:42:48 -0400

    CloseFar completed: the diff of a and c must be greater than 2.

--- src/logic2/CloseFar.java
+++ src/logic2/CloseFar.java
@@ -9,7 +9,10 @@ public class CloseFar {
     public boolean closeFar(int a, int b, int c) {
         boolean cond = false;
         int sumDiff = Math.abs(a - b) + Math.abs(b - c);
-        if (sumDiff > 4) {
+        if (sumDiff > 3 || Math.abs(a - c) > 2) {
+            cond = true;
+        }
+        if (a == b || b == c || a == c) {
             cond = true;
         }
         return cond;
