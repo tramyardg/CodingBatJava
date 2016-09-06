ebd28094dcaeadaf7d8e97318b850fd9d0d72d35
Raymart De Guzman <tramyardg@gmail.com>
2016-09-06 12:21:08 -0400

    Update RepeatSep.java

--- src/string2/RepeatSep.java
+++ src/string2/RepeatSep.java
@@ -10,9 +10,6 @@ public class RepeatSep {
         String newStr = "";
         String sep1 = "";
         for (int i = 0; i < count; i++) {
-            // if((count-1) < i) {
-            // sep1 += sep;
-            // }
             newStr += word + sep;
         }
         return newStr.substring(0, newStr.length() - sep.length());
